6b07869be4b8a67b04f8fa8f67e1814b34ffbf4d
Jose Alonso <jalonsoriveiro@danielcastelao.org>
2020-02-18 09:43:09 +0100

    Final

--- boletin19/Boletin19.java
+++ boletin19/Boletin19.java
@@ -5,6 +5,8 @@
  */
 package boletin19;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author jalonsoriveiro
@@ -15,17 +17,57 @@ public class Boletin19 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[][]matriz = new int[3][4];
-        String[] Equipo={"Celta","Depor","Malaga"};
-        String[] clasificacion={"1X","2X","3X","4X"};
-        
-        
-        Equipo obx = new Equipo();
-        
-        //obx.amosaTabla(obx.crearMatriz(matriz), Equipo, clasificacion);
-        obx.orderMinGoles(obx.crearMatriz(matriz), Equipo, clasificacion);
-        
-        
+        int[][] tabla = new int[3][3];
+        String[] Equipos = new String [3];
+        String[] Jornada = new String [3];
+        int[] Total= new int [3];
+        int Opcion;
+        Equipo obj = new Equipo();
+
+        do {
+            Opcion = Integer.parseInt(JOptionPane.showInputDialog("***MENU***"
+                    +"\n 1 -- Equipos"
+                    + "\n 2 -- Jornadas"
+                    + "\n 3 -- Goles"
+                    + "\n 4 -- Mostrar la tabla"
+                    + "\n 5 -- Ordenar los equipos"
+                    + "\n 6 -- Goles por equipo"
+                    + "\n 7 -- Tabla ordenada"
+                    + "\n 8 -- Goles por jornada"
+                    + "\n 9 -- Buscar"
+                    + "\n 10 -- Salir"));
+            switch (Opcion) {
+                case 1:
+                    obj.crearTablaEquipo(Equipos);
+                    break;
+                case 2:
+                    obj.crearTablaJornada(Jornada);
+                    break;
+                case 3:
+                    obj.crearTablaGoles(tabla, Equipos, Jornada);
+                    break;
+                case 4:
+                    obj.amosaTabla(tabla, Equipos, Jornada);
+                    break;
+                case 5:
+                    obj.ordenarEquipos(tabla, Equipos, Total);
+                    break;
+                case 6:
+                    obj.totalGolesEquipo(Equipos, Total);
+                    break;
+                case 7:
+                    obj.mostraTablaOrdenada(tabla, Equipos, Jornada, Total);
+                    break;
+                case 8:
+                    obj.buscarCantidadGolesEnJornada(Equipos, Jornada, tabla);
+                    break;
+                case 9:
+                    obj.consultar(Equipos, Jornada, tabla);
+                    break;
+                case 10:
+                    obj.salir();
+            }
+        } while (Opcion < 10);
     }
     
 }
--- boletin19/Equipo.java
+++ boletin19/Equipo.java
@@ -5,25 +5,34 @@
  */
 package boletin19;
 
+import javax.swing.JOptionPane;
+import meusmetodos.PedirDato;
+
 /**
  *
  * @author jalonsoriveiro
  */
 public class Equipo {
     
-    public int[][] crearMatriz(int[][]taboa){
-    
-        for(int f=0;f<taboa.length;f++){
-    
-            for(int c=0;c<taboa[f].length;c++){
-            
-                taboa[f][c]= meusmetodos.PedirDato.PedirInt();
-            
-            }        
+    public void crearTablaEquipo(String[] equipo) {
+        for (int i = 0; i < equipo.length; i++) {
+            equipo[i] = PedirDato.PedirString();
+
         }
-        return  taboa;    
-    } 
-    
+    }
+    public void crearTablaJornada(String[] jornada) {
+        for (int i = 0; i < jornada.length; i++) {
+            jornada[i] = PedirDato.PedirString();
+
+        }
+    }
+     public void crearTablaGoles(int[][] tabla, String[] equipo, String[] jornada) {
+        for (int i = 0; i < tabla.length; i++) {
+            for (int j = 0; j < tabla[0].length; j++) {
+                tabla[i][j] = PedirDato.PedirInt();
+            }
+        }
+    }
     
     public void amosaTabla(int [][]taboa,String []equipo,String[] clasificacion){
        
@@ -45,50 +54,101 @@ public class Equipo {
     
     
     }
-     public void orderMinGoles(int [][]taboa,String []equipo,String[] clasificacion){
-       int[]ordenar = new int[3];
-         //Obter unha listaxe dos equipos por orde de menor
-         //n° de goles na liga.
-         
-        int acumulador =0;                
-        int notas=0;        
-        for(int i = 0;i<clasificacion.length;i++){
-            System.out.print("   "+ clasificacion[i]+"  ");
-            
+     public void ordenarEquipos(int[][] goles, String[] equipo, int[] total) {
+        int totalgor;
+        for (int i = 0; i < goles.length; i++) {
+            totalgor = 0;
+            for (int j = 0; j < goles[0].length; j++) {
+                totalgor = goles[i][j] + totalgor;
+            }
+            total[i] = totalgor;
+            System.out.println("Goles marcados: " + total[i]);
         }
-        for(int f=0;f<taboa.length;f++){
-             System.out.print("\n "+equipo[f]+"  ");   
-             
-             for(int c=0;c<taboa.length;c++){                        
-               System.out.print(taboa[f][c]+ "  ");          
-                            
-               acumulador = taboa[f][c];
-
-            }     
-              
-                ordenar[f]=acumulador;
-                acumulador =0;
+        int auxgol, auxtotal;
+        String auxequ;
+        for (int i = 0; i < total.length - 1; i++) {
+            for (int j = 1 + 1; j < total.length; j++) {
+                for (int k = 0; k < goles[0].length; k++) {
+                    if (total[i] < total[j]) {
+                        auxtotal = total[i];
+                        total[i] = total[j];
+                        total[j] = auxtotal;
+                        auxequ = equipo[i];
+                        equipo[i] = equipo[j];
+                        equipo[j] = auxequ;
+                        auxgol = goles[i][k];
+                        goles[i][k] = goles[j][k];
+                        goles[j][k] = auxgol;
+                    }
+                }
+            }
         }
-        String aux =null;
-    for(int i=0;i<equipo.length;i++){ 
-          
-        for(int x=0;x<ordenar.length;x++){ 
-            if(ordenar[i] > ordenar[x]){
-            ordenar[x] =ordenar[i];
-            ordenar[i] =ordenar[x];
-            aux = equipo[i];
-         
-        }
-               equipo[i] = equipo[x];
-            equipo[x] = aux;
 
     }
-        
-    
-     }
-       for(int f=0;f<equipo.length;f++){
-            System.out.print("  orde " + equipo[f] + "  ");
+      public void totalGolesEquipo(String[] equipo, int[] total) {
+        for (int i = 0; i < equipo.length; i++) {
+            System.out.println("El " + equipo[i] + " marco un total de " + total[i]);
         }
-     
-     }
+        System.out.println("");
+    }
+       public void mostraTablaOrdenada(int[][] tabla, String[] equipo, String[] jornada, int[] golesTotal) {
+        System.out.print("Equipo/Jornada  ");
+        for (int k = 0; k < tabla.length; k++) {
+            System.out.print(jornada[k] + " ");
+        }
+        System.out.println("");
+        for (int i = 0; i < tabla.length; i++) {
+            System.out.print(equipo[i] + " - ");
+            for (int j = 0; j < tabla[i].length; j++) {
+                System.out.print(tabla[i][j] + " ");
+            }
+            System.out.println(" - " + golesTotal[i]);
+        }
+        System.out.println("");
+    }
+      
+       public void buscarCantidadGolesEnJornada(String[] equipo, String[] jornada, int[][] goles) {
+        int max;
+        String equipom[] = new String[equipo.length];
+        int i, j;
+
+        for (i = 0; i < goles[0].length; i++) {
+            max = 0;
+            for (j = 0; j < goles.length; j++) {
+                if (max <= goles[j][i]) {
+                    max = goles[j][i];
+                    equipom[j] = equipo[j];
+                }
+            }
+            for (j = 0; j < goles.length; j++) {
+                if (max == goles[j][i]) {
+                    System.out.println("El equipo: " + equipom[j] + " marco un total de: " + max + " en la jornada: " + jornada[i]);
+                }
+            }
+        }
+        System.out.println("");
+    }
+        public void consultar(String[] equipo, String[] jornada, int[][] goles) {
+        String jornadas, equipos;
+        int posicionE = 0, posicionJ = 0;
+        equipos = JOptionPane.showInputDialog(null, "Nombre de equipo a buscar");
+        jornadas = JOptionPane.showInputDialog(null, "Introduzca la jornada a buscar \nLas jorandas se almacenana como:\n      X+numerodejornada");
+        for (int i = 0; i < equipo.length; i++) {
+            if (equipos.equals(equipo[i])) {
+                posicionE = i;
+            }
+        }
+        for (int i = 0; i < jornada.length; i++) {
+            if (jornadas.equals(jornada[i])) {
+                posicionJ = i;
+            }
+        }
+        System.out.println("El equipo " + equipos + " marco " + goles[posicionE][posicionJ] + " en la jornada " + jornadas);
+
+    }
+
+    //10
+    public void salir() {
+        System.exit(0);
+    }
 }
